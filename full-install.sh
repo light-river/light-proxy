c8a2e0f6b1eb0da9bc11c79d4a09067c0ba4993a
solo-fish <solo.fish@pm.me>
2021-05-12 09:56:31 -0700

    xargs, ref pathing, fixed default.cfg

--- full-install.sh
+++ full-install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Args
+# 1) domain name you're signing | default: proxy.localhost
+# 2) output parent folder for certs | default: /.light-certs/proxy.localhost/*
+host_name=${1:-proxy.localhost}
+cert_root_path="${2:-"/.light-certs/"}$host_name"
+echo -e "configured with \n\t host_name: $host_name \n\t cert_path: $cert_root_path"
+
 # Install light-ca
 curl -sL https://github.com/light-river/light-ca/releases/download/latest/light-ca.tar.gz | tar zx && sudo mv ./light-ca /usr/bin/light-ca
-light-ca --domains 'proxy.localhost' 
-light-ca --domains '*.proxy.localhost'
+sudo light-ca --domains "$host_name"
+sudo light-ca --domains "*.$host_name"
+
+# Move certs
+sudo mkdir -p cert_root_path
+sudo mv "./$host_name/*" "$cert_root_path"
+echo "certs installed at $cert_root_path"
 
 # Install Apache2
 sudo apt-get update
@@ -13,7 +25,7 @@ sudo apt-get install apache2
 sudo systemctl stop apache2.service
 sudo systemctl start apache2.service
 sudo systemctl enable apache2.service
-sudo systemctl stop apache2.service
+sudo systemctl restart apache2.service
 
 # Install apache proxy modules
 sudo a2enmod ssl
@@ -22,7 +34,7 @@ sudo a2enmod proxy_http
 sudo a2enmod proxy_balancer
 sudo a2enmod proxy_wstunnel
 sudo a2enmod proxy_connect
-sudo systemctl restart apache2
+sudo systemctl restart apache2.service
 
 # Copy over template configuration to Apache
 sudo cp ./template-configs/default.cfg /etc/apache2/sites-available/Apache2Proxy.conf
